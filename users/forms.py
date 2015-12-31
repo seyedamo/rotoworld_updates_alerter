d9fe55ab60949a7820d8026a128b89b7c2acbebb
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-31 18:53:52 -0500

    update

--- users/forms.py
+++ users/forms.py
@@ -3,5 +3,7 @@ from users.models import User
 
 
 class SignUpForm(forms.ModelForm):
-    model = User
-    email_address = forms.EmailField(label="email_address")
+
+    class Meta:
+        model = User
+        fields = ['email_address']
